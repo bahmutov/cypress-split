2ae66d310d5bc3b754bcea309a10db1d2ef4f8f5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-10-11 10:10:55 -0400

    fix: use absolute spec list to avoid confusion

---
+++ examples/user-spec-pattern.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require('cypress')
+const cypressSplit = require('../src')
+
+module.exports = defineConfig({
+  e2e: {
+    // baseUrl, etc
+    supportFile: false,
+    fixturesFolder: false,
+    excludeSpecPattern: '*.hot-update.js',
+    setupNodeEvents(on, config) {
+      console.log('cwd is', process.cwd())
+
+      // user sets their own custom specPattern list of specs
+      // make sure the list of specs is relative to the folder
+      // with the Cypress config file!
+      config.specPattern = [
+        '../cypress/e2e/spec-c.cy.js',
+        '../cypress/e2e/spec-d.cy.js',
+        '../cypress/e2e/spec-e.cy.js',
+      ]
+      cypressSplit(on, config)
+      // IMPORTANT: return the config object
+      return config
+    },
+  },
+})
