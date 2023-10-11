3e01b45de3828f7532fa94a048474453f8f77d4a
Morten Larsen <baunegaard@live.dk>
2023-10-11 20:44:55 +0200

    Fix typings for default export

--- src/types.d.ts
+++ src/types.d.ts
@@ -4,7 +4,11 @@ import type Cypress from 'cypress'
  * Initializes the cypress-split plugin using Cypress config values.
  * @see https://github.com/bahmutov/cypress-split
  */
-export default function cypressSplit(
-  on: Cypress.PluginEvents,
-  config: Cypress.PluginConfigOptions,
-): void
+interface CypressSplit {
+  (
+    on: Cypress.PluginEvents,
+    config: Cypress.PluginConfigOptions,
+  ): void;
+}
+declare var cypressSplit: CypressSplit
+export = cypressSplit
