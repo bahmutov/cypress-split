71c1ac860400b689b19b06e2a55d3d62e790d0a8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-10-16 10:39:45 -0400

    feat: add merging of timings, still to be finished

--- bin/merge.js
+++ bin/merge.js
@@ -2,6 +2,7 @@
 
 const arg = require('arg')
 const debug = require('debug')('cypress-split')
+const globby = require('globby')
 
 const label = 'cypress-split-merge'
 
@@ -10,6 +11,14 @@ const args = arg({
   '--parent-folder': String,
 })
 debug('args %o', args)
+if (!args['--parent-folder']) {
+  console.error('Missing --parent-folder')
+  process.exit(1)
+}
+if (!args['--split-file']) {
+  console.error('Missing --split-file')
+  process.exit(1)
+}
 
 console.log(
   '%s finding all timings files %s/**/%s',
@@ -17,3 +26,10 @@ console.log(
   args['--parent-folder'],
   args['--split-file'],
 )
+
+const files = globby.sync('**/' + args['--split-file'], {
+  sort: true,
+  cwd: args['--parent-folder'],
+})
+console.log('%s found %d file(s)', label, files.length)
+debug(files)
