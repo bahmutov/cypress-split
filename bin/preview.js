442a2340918dba41155817ab52bbd33e6cafdb8c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-12-28 12:57:43 -0500

    demo preview

--- bin/preview.js
+++ bin/preview.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
+// @ts-check
+
 const arg = require('arg')
 const debug = require('debug')('cypress-split')
 const label = 'cypress-split-preview'
 const { version } = require('../package.json')
-const { parseSplitInputs, getSpecsToSplit } = require('../src/parse-inputs')
+const { getSpecsToSplit } = require('../src/parse-inputs')
+const { splitSpecsLogic } = require('../src/utils')
 
 console.log('%s Version %s', label, version)
 
@@ -18,3 +21,17 @@ console.log('%s split %d ways', label, args['--split'])
 const specs = getSpecsToSplit(process.env)
 console.log('%s found %d specs', label, specs.length)
 debug(specs)
+
+if (typeof args['--split'] !== 'number') {
+  throw new Error('Please provide --split N')
+}
+
+for (let k = 0; k < args['--split']; k += 1) {
+  splitSpecsLogic({
+    specs,
+    splitN: args['--split'],
+    splitIndex: k,
+    splitFileName: args['--split-file'],
+    label,
+  })
+}
