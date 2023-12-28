aece27a6fec075ee8b164b4bd3b3ad8c3634f8b7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-12-28 10:48:43 -0500

    update the variable name

--- src/parse-inputs.js
+++ src/parse-inputs.js
@@ -1,9 +1,11 @@
+// @ts-check
+
 function parseSplitInputs(env = {}, configEnv = {}) {
-  let SPLIT = process.env.SPLIT || config.env.split || config.env.SPLIT
-  let SPLIT_INDEX = process.env.SPLIT_INDEX || config.env.splitIndex
-  let SPLIT_FILE = process.env.SPLIT_FILE || config.env.splitFile
+  let SPLIT = process.env.SPLIT || configEnv.split || configEnv.SPLIT
+  let SPLIT_INDEX = process.env.SPLIT_INDEX || configEnv.splitIndex
+  let SPLIT_FILE = process.env.SPLIT_FILE || configEnv.splitFile
   let SPLIT_OUTPUT_FILE =
-    process.env.SPLIT_OUTPUT_FILE || config.env.splitOutputFile || SPLIT_FILE
+    process.env.SPLIT_OUTPUT_FILE || configEnv.splitOutputFile || SPLIT_FILE
 
   return { SPLIT, SPLIT_INDEX, SPLIT_FILE, SPLIT_OUTPUT_FILE }
 }
