7c209a205f2a1b693501867cd1f7b3a572a7804c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-01-24 12:10:25 -0500

    feat: resolve user-specified wildcard patterns

--- test/parse-inputs.test.js
+++ test/parse-inputs.test.js
@@ -17,3 +17,33 @@ test('getSpecsToSplit spec pattern', (t) => {
     'cypress/integration/spec-b.js',
   ])
 })
+
+test('getSpecsToSplit spec pattern with wildcards', (t) => {
+  const specs = getSpecsToSplit({
+    SPEC: 'cypress/e2e/spec-*.cy.js',
+  })
+  // finds all specs matching the glob pattern
+  const relativeSpecs = toRelative(specs)
+  t.deepEqual(relativeSpecs, [
+    'cypress/e2e/spec-a.cy.js',
+    'cypress/e2e/spec-b.cy.js',
+    'cypress/e2e/spec-c.cy.js',
+    'cypress/e2e/spec-d.cy.js',
+    'cypress/e2e/spec-e.cy.js',
+  ])
+})
+
+test('getSpecsToSplit spec pattern with subfolder wildcards', (t) => {
+  const specs = getSpecsToSplit({
+    SPEC: 'cypress/**/spec-*.cy.js',
+  })
+  // finds all specs matching the glob pattern
+  const relativeSpecs = toRelative(specs)
+  t.deepEqual(relativeSpecs, [
+    'cypress/e2e/spec-a.cy.js',
+    'cypress/e2e/spec-b.cy.js',
+    'cypress/e2e/spec-c.cy.js',
+    'cypress/e2e/spec-d.cy.js',
+    'cypress/e2e/spec-e.cy.js',
+  ])
+})
